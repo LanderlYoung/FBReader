804135e28d799a830307d201f50d7687fc3772f9
taylorcyang(杨超) <taylorcyang@tencent.com>
2019-04-16 13:37:09 +0800

    compiles on macos

--- makefiles/arch/macosx.mk
+++ makefiles/arch/macosx.mk
@@ -14,28 +14,34 @@ APPIMAGEDIR_MACRO = $(SHAREDIR_MACRO)/icons
 
 ZLSHARED = no
 
-TOOLSPATH = /Developer/usr/bin
-CC = $(TOOLSPATH)/gcc-4.2
-AR = $(TOOLSPATH)/ar rsu
-LD = $(TOOLSPATH)/g++-4.2
+SYSROOT = $(xcode-select -p)
+SYSROOT=/Applications/Xcode.app/Contents/Developer
+SYSROOT=/
+TOOLSPATH = $(SYSROOT)/usr/bin
+CC = $(TOOLSPATH)/gcc
+AR = /usr/bin/ar rsu
+LD = $(TOOLSPATH)/g++
 
-MACOS_VERSION = 10.5
+MACOS_VERSION = 10.14
 
 #ARCH_FLAGS = -arch x86_64 -arch i386 -arch ppc7400 -arch ppc64
-ARCH_FLAGS = -arch i386 -arch ppc7400
+ARCH_FLAGS = -arch x86_64
 CFLAGS_NOARCH = \
 	-fmessage-length=0 -pipe -fpascal-strings -fasm-blocks \
 	-mdynamic-no-pic -W -Wall \
-	-isysroot /Developer/SDKs/MacOSX$(MACOS_VERSION).sdk \
-	-fvisibility=hidden -fvisibility-inlines-hidden \
+	-isysroot $(SYSROOT) \
 	-mmacosx-version-min=$(MACOS_VERSION) \
-	-gdwarf-2
+	-gdwarf-2 \
+	-stdlib=libc++
+	#-fvisibility=hidden -fvisibility-inlines-hidden \
 CFLAGS = $(ARCH_FLAGS) $(CFLAGS_NOARCH)
 LDFLAGS = $(ARCH_FLAGS) \
-	-isysroot /Developer/SDKs/MacOSX$(MACOS_VERSION).sdk \
-	-mmacosx-version-min=$(MACOS_VERSION)
-EXTERNAL_INCLUDE = -I/usr/local/include
-EXTERNAL_LIBS = -liconv
+	-mmacosx-version-min=$(MACOS_VERSION) \
+	-isysroot $(SYSROOT)
+EXTERNAL_INCLUDE = -I/usr/local/include -I$(ROOTDIR)/libs/macosx/include
+EXTERNAL_LIBS = -L$(ROOTDIR)/libs/macosx/lib    \
+                -L/usr/lib                      \
+                -liconv -llinebreak -lfribidi
 
 UILIBS = -framework Cocoa $(ROOTDIR)/zlibrary/ui/src/cocoa/application/CocoaWindow.o $(ROOTDIR)/zlibrary/ui/src/cocoa/library/ZLCocoaAppDelegate.o
 
